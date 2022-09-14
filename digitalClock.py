75edc852a754a5d25ddae60bd925d91a2cead14e
Vaishnavi <vaishnaviic02@gmail.com>
2022-09-15 00:03:12 +0530

    changed to RED screen

--- digitalClock.py
+++ digitalClock.py
@@ -5,9 +5,9 @@ window = Tk()
 window.title("Digital Clock")
 window.geometry("600x300")
 
-window.configure(bg="steelblue")
+window.configure(bg="red")
 
-label = Label(window, text="Heyaaa", font=("Arial Black",78,"bold"),bg="steelblue" ,fg="white")
+label = Label(window, text="Heyaaa", font=("Arial Black",78,"bold"),bg="red" ,fg="white")
 label.pack(pady=100)
 
 window.mainloop()
